5b403a4d03d29af5f6f6c00551dd8f3d3aa491b6
Uday Chandra <uday@standardapplied.com>
2026-09-24 00:58:56 +0000

    test: provision the upgrade box once its systemd has booted

--- sail-infra/src/test/java/ai/singlr/sail/commands/UpgradeE2EIT.java
+++ sail-infra/src/test/java/ai/singlr/sail/commands/UpgradeE2EIT.java
@@ -128,6 +128,7 @@ class UpgradeE2EIT extends AbstractIncusIT {
     ensureIncusOrSkip();
     try {
       launchPrepared(BOX);
+      root("systemctl is-system-running --wait");
       push(binaries.released(), INSTALLED);
       push(binaries.candidate(), STAGED);
       rootOk(PROVISION_MAIN);
